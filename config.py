840131167296799cb3fa0c98fcba63c11388e11a
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-27 17:00:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 TOKEN = '6913439784:AAEhIjZm-oSavOqtR1RXHQ3F__KqDDfCQRw'
-db_url = 'postgresql://vegetarobot_owner:Wj5y7eJmpAxQ@ep-fancy-dream-a1ermpse.ap-southeast-1.aws.neon.tech/vegetarobot?sslmode=require'
+DB_URL = 'postgresql://vegetarobot_owner:Wj5y7eJmpAxQ@ep-fancy-dream-a1ermpse.ap-southeast-1.aws.neon.tech/vegetarobot?sslmode=require'
+SUPPORT_CHAT = 'NandhaChat'

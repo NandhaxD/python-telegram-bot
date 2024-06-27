d8dcb7d4c9a9d01d71a5bc26394bcf44cea4d6d1
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-27 16:25:05 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN = '6913439784:AAEhIjZm-oSavOqtR1RXHQ3F__KqDDfCQRw'
+db_url = 'postgresql://vegetarobot_owner:Wj5y7eJmpAxQ@ep-fancy-dream-a1ermpse.ap-southeast-1.aws.neon.tech/vegetarobot?sslmode=require'

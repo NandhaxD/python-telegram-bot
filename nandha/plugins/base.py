238e7d44330b15726545082f28cacc2e4113c1ab
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-27 23:23:42 +0530

    Update base.py

--- nandha/plugins/base.py
+++ nandha/plugins/base.py
@@ -6,10 +6,14 @@ from telegram.ext import CommandHandler, ContextTypes
 from nandha import app, SUPPORT_CHAT
 from nandha.sql.users import add_user, get_all_users
 from nandha.sql.chats import get_all_chats, add_chat
+from nandha.helpers.decorator import command
 
+
+@command('start')
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     bot = context.bot
+    message = update.effective_message
     chat = update.effective_chat
     user = update.effective_user
     mention = helpers.mention_markdown(user_id=user.id, name=user.first_name, version=2)
@@ -57,12 +61,9 @@ f"""
 
     buttons = InlineKeyboardMarkup(keyboard)
     
-    await bot.send_message(
-        chat_id=chat.id,
+    await message.reply_text(
         text=(f"*Hello there {mention}, I'm Simple bot made by @NandhaBots using [PythonTelegramBot](https://docs.python-telegram-bot.org) Library\.*"),
         parse_mode=constants.ParseMode.MARKDOWN_V2,
         reply_markup=buttons
     )
 
-start_handler = CommandHandler('start', start)
-app.add_handler(start_handler)

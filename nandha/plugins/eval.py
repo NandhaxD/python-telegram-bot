18aea657c416e5a200ee2414811c0eed63103864
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-28 00:15:41 +0530

    Update eval.py

--- nandha/plugins/eval.py
+++ nandha/plugins/eval.py
@@ -82,7 +82,6 @@ def cleanup_code(code):
 
 
 async def do(func, bot, update):
-    log_input(update)
     content = update.message.text.split(maxsplit=1)[1]
     body = cleanup_code(content)
     env = namespace_of(update.message.chat_id, update, bot)

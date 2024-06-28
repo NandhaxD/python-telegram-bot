5ec4eaf4d6eccd1cee00dfa55fb31d3ccd62cff5
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-28 19:09:36 +0530

    Update admin.py

--- nandha/plugins/admin.py
+++ nandha/plugins/admin.py
@@ -10,6 +10,67 @@ from nandha.helpers.decorator import command, admin_check
 
 
 
+@command('unban')
+@admin_check('can_restrict_members')
+async def UnBanChatMember(update, context):
+    message = update.message
+    reply = message.reply_to_message
+    chat = message.chat
+    user = message.from_user
+    bot = context.bot
+    
+    if getattr(reply, 'sender_chat', None):
+        sender_chat = reply.sender_chat
+        try:
+           success = await bot.unban_chat_sender_chat(
+               chat_id=chat.id,
+               sender_chat_id=sender_chat.id
+        )
+           if success:
+                return await message.reply_text(
+text=(
+f"""
+<b>⚡ Channel {sender_chat.title} UnBanned in {chat.title}.</b>
+"""), parse_mode=constants.ParseMode.HTML
+            )
+        except TelegramError as e:
+            return await message.reply_text(
+              text=f"❌ Error: {str(e)}"
+        )
+
+            
+               
+    user_id = extract_user(message)
+  
+    if not user_id or user_id == user.id:
+        return await message.reply_text(
+           "Reply to a user or provide their id / mention to UnBan !"
+        )
+      
+    try:
+      
+        member = await bot.get_chat_member(chat.id, user_id)
+      
+        success = await bot.unban_chat_member(
+             chat_id=chat.id,
+             user_id=member.user.id
+        )
+        if success:
+            member_mention = mention_html(member.user.id, member.user.first_name)
+            return await message.reply_text(
+text=(
+f"""
+<b>⚡ {'Bot' if member.user.is_bot else 'User'} {member_mention} UnBanned in {chat.title}.</b>
+"""), parse_mode=constants.ParseMode.HTML
+            )
+    except error.TelegramError as e:
+        return await message.reply_text(
+          text=f"❌ Error: {str(e)}"
+        )
+
+
+
+
 @command('ban')
 @admin_check('can_restrict_members')
 async def BanChatMember(update, context):
@@ -44,7 +105,7 @@ f"""
   
     if not user_id or user_id == user.id:
         return await message.reply_text(
-           "Reply to a user or provide their id / mention to ban !"
+           "Reply to a user or provide their id / mention to Ban !"
         )
       
     try:

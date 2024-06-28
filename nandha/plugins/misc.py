1b462276b39ed1f29542c61bcaf32f03ecb0d632
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-28 22:36:44 +0530

    Update misc.py

--- nandha/plugins/misc.py
+++ nandha/plugins/misc.py
@@ -8,52 +8,49 @@ from nandha.helpers.decorator import command
 
 @command(('tm','tgm'))
 async def Telegraph(update, context):
-     message = update.message
-     bot = context.bot
+    message = update.message
+    bot = context.bot
 
-     api_url = "https://telegra.ph/upload"
+    api_url = "https://telegra.ph/upload"
 
-     reply = message.reply_to_message
-     if not reply:
-          return await message.reply_text(
-           text="⚡ Reply to the animation (GIF) or a photo to upload in graph.org"
-         )
-       
-     if reply.photo:
-         file_name = f"{str(uuid.uuid4())}.jpeg"
-         media_type = "image/jpeg"
-         file_id = reply.photo[3].file_id
-       
-     elif reply.animation:
-         file_name = reply.animation.file_name
-         media_type = reply.animation.mime_type
-         file_id = reply.animation.file_id
-       
-     else:
-         return await message.reply_text(
-           text="⚡ Reply to the animation (GIF) or a photo to upload in grap.org"
-         )
-       
-     msg = await message.reply_text("Downloading...")
-     file = await bot.get_file(file_id)
-     file_path = await file.download_to_drive(
-             custom_path=file_name
-     )
-     
-
-     mpwriter = MultipartWriter()
-     part = mpwriter.append(Body(file_path, filename=file_name, contentType=media_type))
-     part.set_content_disposition('form-data', filename=file_name)
-     
-     async with session.post(api_url, data=mpwriter) as response:
-        if response.status == 200:
-            data = await response.json()
-            if isinstance(data, dict):
-                return await msg.edit_text("❌ Image was upload but error while getting file link.")
-            src = data[0].get('src')
-            url = 'https://graph.org' + src
-            return await msg.edit_text(url)
-        else:
-           return await msg.edit_text(
-               text=f"❌ can't upload status code: {str(response.status)}"
-           )
+    reply = message.reply_to_message
+    if not reply:
+        return await message.reply_text(
+            text="⚡ Reply to the animation (GIF) or a photo to upload in graph.org"
+        )
+    
+    if reply.photo:
+        file_name = f"{str(uuid.uuid4())}.jpeg"
+        media_type = "image/jpeg"
+        file_id = reply.photo[3].file_id
+    elif reply.animation:
+        file_name = reply.animation.file_name
+        media_type = reply.animation.mime_type
+        file_id = reply.animation.file_id
+    else:
+        return await message.reply_text(
+            text="⚡ Reply to the animation (GIF) or a photo to upload in grap.org"
+        )
+    
+    msg = await message.reply_text("Downloading...")
+    file = await bot.get_file(file_id)
+    file_path = await file.download_to_drive(
+        custom_path=file_name
+    )
+    
+    async with aiohttp.ClientSession() as s:
+        with open(file_path, 'rb') as f:
+            mpwriter = MultipartWriter()
+            part = mpwriter.append(f, headers={'Content-Type': media_type, 'Content-Disposition': f'form-data; name="file"; filename="{file_name}"'})
+            async with s.post(api_url, data=mpwriter) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    if isinstance(data, dict):
+                        return await msg.edit_text("❌ Image was upload but error while getting file link.")
+                    src = data[0].get('src')
+                    url = 'https://graph.org' + src
+                    return await msg.edit_text(url)
+                else:
+                    return await msg.edit_text(
+                        text=f"❌ can't upload status code: {str(response.status)}"
+                    )

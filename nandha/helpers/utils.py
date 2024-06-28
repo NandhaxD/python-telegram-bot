bcc2d390d01ac1bb94bcbf2572f8316fd74b27f5
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-28 17:56:57 +0530

    Update utils.py

--- nandha/helpers/utils.py
+++ nandha/helpers/utils.py
@@ -8,19 +8,16 @@ from telegram.constants import MessageEntityType
 def extract_user(message):
     text = message.text
     user_id = None
-
-    if message.entities and len(text.split()) >= 2:
+    reply = message.reply_to_message
+  
+    if len(text.split()) >= 2:
         text_mention_user_ids = [entity.user.id for entity in message.entities if entity.type == MessageEntityType.TEXT_MENTION]
         if text_mention_user_ids:
             user_id = text_mention_user_ids[0]
-        else:
-            mention_usernames = [text.split()[1] for entity in message.entities if entity.type == MessageEntityType.MENTION]
-            if mention_usernames:
-                user_id = mention_usernames[0]
+    elif reply and reply.from_user:
+          user_id = reply.from_user.id
     else:
-        reply = message.reply_to_message
-        if reply and reply.from_user and not reply.from_user.is_bot:
-            user_id = reply.from_user.id
-
+          user_id = message.from_user.id
+      
     return user_id
     

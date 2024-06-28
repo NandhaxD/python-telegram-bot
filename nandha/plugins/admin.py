3ce45eae21e8e7a7d50adf442428404810dd736d
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-28 09:39:02 +0530

    Update admin.py

--- nandha/plugins/admin.py
+++ nandha/plugins/admin.py
@@ -9,12 +9,13 @@ from nandha.helpers.decorator import command, admin_check
 @command('del')
 @admin_check('can_delete_messages')
 async def delete(update: Update, context: ContextTypes.DEFAULT_TYPE):
-      message = update.effective_message
-      reply = message.reply_to_message
-      if reply:
-         await reply.delete()
-         await message.delete()
-      else:
-          return await message.reply_text(
-              text="What should i delete ?"
-          )
+    message = update.effective_message
+    reply = message.reply_to_message
+    if reply:
+        try:
+            await reply.delete()
+            await message.delete()
+        except TelegramError as e:
+            return await message.reply_text("Error: {}".format(e))
+    else:
+        return await message.reply_text("What should I delete?")

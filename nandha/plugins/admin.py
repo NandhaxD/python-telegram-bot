2a9ce461e640a614a849d66fdcb7a7df5a068f83
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-28 14:22:58 +0530

    Update admin.py

--- nandha/plugins/admin.py
+++ nandha/plugins/admin.py
@@ -1,9 +1,33 @@
 
 
 from telegram.ext import ContextTypes, CallbackContext
-from telegram import Update
+from telegram import Update, error, constants
+from telegram.helpers import mention_html
 
 from nandha.helpers.decorator import command, admin_check
+
+
+@command(('adminlist','admins'))
+@admin_check(None)
+async def AdminList(update, context):
+    message = update.message
+    chat = message.chat
+    bot = context.bot
+    try:
+      admins = await bot.get_administrators()
+    except TelegramError as e:
+        return await message.reply_text(
+            text=f"❌ Error: {str(e)}"
+        )
+
+    text = f"<b>👮 Admins in {chat.title}</b>:"
+    for user in admins:
+         text += "➣ " + mention_html(user.id, user.first_name)
+    return await message.reply_text(
+         text=text, parse_mode=constants.ParseMode.HTML)
+    
+
+
 
 
 @command('del')
@@ -15,7 +39,7 @@ async def delete(update: Update, context: CallbackContext):
         try:
             await reply.delete()
             await message.delete()
-        except TelegramError as e:
+        except error.TelegramError as e:
             return await message.reply_text("Error: {}".format(e))
     else:
         return await message.reply_text("What should I delete?")

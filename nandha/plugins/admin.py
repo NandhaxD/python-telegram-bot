f1c68867b78ffe1418ce6650fdeb6786f1711b9c
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-28 18:43:53 +0530

    Update admin.py

--- nandha/plugins/admin.py
+++ nandha/plugins/admin.py
@@ -8,24 +8,42 @@ from nandha.helpers.decorator import command, admin_check
 
 
 
+
+
 @command('ban')
 @admin_check('can_restrict_members')
 async def BanChatMember(update, context):
     message = update.message
+    reply = message.reply_to_message
     chat = message.chat
-    user = message.from_user
     bot = context.bot
-  
-    user_id = extract_user(message)
-    if not user_id or user_id == user.id:
-        return await message.reply_text(
-           "Reply to a user or give their id / mention to ban !!"
+    
+    if getattr(reply, 'sender_chat', None):
+        sender_chat = reply.sender_chat
+        success = await bot.ban_chat_sender_chat(
+             chat_id=chat.id,
+             sender_chat_id=sender_chat.id
         )
+        if success:
+            return await message.reply_text(
+text=(
+f"""
+<b>⚡ Channel {sender_chat.title} Banned in {chat.title}.</b>
+"""), parse_mode=constants.ParseMode.HTML
+            )
+               
+    user_id = extract_user(message)
+  
+    if not user_id:
+        return await message.reply_text(
+           "Reply to a user or provide their id / mention to ban !"
+        )
+      
     try:
         member = await bot.get_chat_member(chat.id, user_id)
         success = await bot.ban_chat_member(
              chat_id=chat.id,
-             user_id=member.id
+             user_id=member.user.id
         )
         if success:
             member_mention = mention_html(member.user.id, member.user.first_name)

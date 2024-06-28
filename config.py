bc994939e1bc960d6aa17fcc6d464412e222d2e0
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-28 07:41:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN = '6913439784:AAEhIjZm-oSavOqtR1RXHQ3F__KqDDfCQRw'
+DB_URL = 'postgresql://vegetarobot_owner:Wj5y7eJmpAxQ@ep-fancy-dream-a1ermpse.ap-southeast-1.aws.neon.tech/vegetarobot?sslmode=require'
+SUPPORT_CHAT = '@NandhaChat'
+DEV_LIST = [5696053228]
+

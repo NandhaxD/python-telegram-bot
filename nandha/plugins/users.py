c18994cfb2afe55543fa1c356fe673a7a8e55b0e
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-29 23:52:46 +0530

    Update users.py

--- nandha/plugins/users.py
+++ nandha/plugins/users.py
@@ -17,17 +17,17 @@ async def TelegramID(update, context):
   
      text = (
 f"""
-*You're Tg ID*: `{message.sender_chat.id if message.sender_chat else message.from_user.id}`
-*Chat ID*: `{message.chat.id}`
-*Msg ID*: `{message.message_id}`
+*🚹 You're Tg ID*: `{message.sender_chat.id if message.sender_chat else message.from_user.id}`
+*🗨️ Chat ID*: `{message.chat.id}`
+*📝 Msg ID*: `{message.message_id}`
 """
 )  
      if reply:
-          text += f"\n*Replied Tg ID*: `{reply.sender_chat.id if reply.sender_chat else reply.from_user.id}`"
-          text += f"\n*Replied Msg ID*: `{reply.message_id}`"
-          media_id = get_media_id(reply)
-          if media_id:
-               text += f"\n*Replied Media ID*: `{media_id}`"
+          text += f"\n*🚹 Replied Tg ID*: `{reply.sender_chat.id if reply.sender_chat else reply.from_user.id}`"
+          text += f"\n*📝 Replied Msg ID*: `{reply.message_id}`"
+          media_type, media_id = get_media_id(reply)
+          if media_type and media_id:
+               text += f"\n*📁 Replied {media_type} ID*: `{media_id}`"
      return await message.reply_text(
          text=text, parse_mode=constants.ParseMode.MARKDOWN
      )

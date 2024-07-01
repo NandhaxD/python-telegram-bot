49e46b167abad6a9c51d0d52fcd2026488c8cd9e
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-07-01 10:53:43 +0530

    Update decorator.py

--- nandha/helpers/decorator.py
+++ nandha/helpers/decorator.py
@@ -2,7 +2,7 @@
 
 
 from functools import wraps
-from telegram import ChatMemberOwner, constants
+from telegram import ChatMemberOwner, ChatMemberAdministrator, constants
 from telegram.ext import CommandHandler, filters
 from nandha import app, DEV_LIST, BOT_ID
 
@@ -18,7 +18,8 @@ def command(command, filters=None, block=False):
 
 
 
-def admin_check(permission: str = False):
+
+async def admin_check(permission: str = None):
     def decorator(func):
         @wraps(func)
         async def wrapper(update, context, *args, **kwargs):
@@ -26,49 +27,51 @@ def admin_check(permission: str = False):
             user = update.effective_user
             message = update.effective_message
             
-            if getattr(message, 'sender_chat'): 
-                return
+            if getattr(message, 'sender_chat', None): 
+                 return
             
             STATUS = [
-               constants.ChatMemberStatus.ADMINISTRATOR, 
-               constants.ChatMemberStatus.OWNER
+                constants.ChatMemberStatus.ADMINISTRATOR, 
+                constants.ChatMemberStatus.OWNER
             ]
-          
-            user = await chat.get_member(user.id)
-            bot = await chat.get_member(BOT_ID)
-          
-            if user.status in STATUS and bot.status in STATUS:
-                if not isinstance(user, ChatMemberOwner):
-                    if permission and (not getattr(user, permission) and not getattr(bot, permission)):
-                        if not getattr(user, permission):
+            
+            user_member = await chat.get_member(user.id)
+            bot_member = await chat.get_member(context.bot.id)
+            
+            if user_member.status in STATUS and bot_member.status in STATUS:
+                if isinstance(user_member, ChatMemberOwner):
+                    return await func(update, context, *args, **kwargs)
+                elif isinstance(user_member, ChatMemberAdministrator):
+                    if permission:
+                        user_permission = getattr(user_member, permission, False)
+                        bot_permission = getattr(bot_member, permission, False)
+                        if not user_permission:
                             await message.reply_text(
-                                "You are missing {} permission.".format(permission)
+                                f"You are missing the `{permission}` permission."
                             )
                             return
-                        else:
+                        elif not bot_permission:
                             await message.reply_text(
-                                "The bot is missing {} permission.".format(permission)
+                                f"The bot is missing the `{permission}` permission."
                             )
                             return
-                          
-                    else:
-                       return await func(update, context, *args, **kwargs)
-                      
-                else:
-                   return await func(update, context, *args, **kwargs)
+                    return await func(update, context, *args, **kwargs)
             else:
-                if user.status not in STATUS:
+                if user_member.status not in STATUS:
                     await message.reply_text(
                         "You are not an admin in this chat."
                     )
-                    return
                 else:
                     await message.reply_text(
                         "The bot is not an admin in this chat."
                     )
-                    return
+                return
+                
         return wrapper
     return decorator
+    
+
+
   
 
     

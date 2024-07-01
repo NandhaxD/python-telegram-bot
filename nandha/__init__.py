2ab04e5d8fcda4ba0f6e57ece79387cccd05bb9c
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-07-01 09:59:38 +0530

    Update __init__.py

--- nandha/__init__.py
+++ nandha/__init__.py
@@ -21,7 +21,7 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 
 
 # Clients
-persistence = PicklePersistence(filepath='db.py')
+persistence = PicklePersistence(filepath='db')
 app = ApplicationBuilder().token(TOKEN).persistence(persistence).build()
 
 aiohttpsession = aiohttp.ClientSession()

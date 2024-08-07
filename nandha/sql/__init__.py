809ce6eb326508a3e8d963079e1b9bc88fccf8a2
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-08-07 11:08:50 +0530

    Update __init__.py

--- nandha/sql/__init__.py
+++ nandha/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 # Define the database URL
-DB_URL = "asyncpg+postgresql://postgres.lfxzalulgmqdalctysxe:PkAkZwH2yHnweNsT@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
+DB_URL = "postgresql+asyncpg://postgres.lfxzalulgmqdalctysxe:PkAkZwH2yHnweNsT@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
 
 # Create the async engine
 engine = create_async_engine(DB_URL, echo=True)

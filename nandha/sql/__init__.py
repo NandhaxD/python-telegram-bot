e8f8857aacf9a2d7c5e6af59fe60ad57c7061596
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-08-07 10:31:21 +0530

    Update __init__.py

--- nandha/sql/__init__.py
+++ nandha/sql/__init__.py
@@ -1,20 +1,11 @@
-from nandha import DB_URL
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
-# Initialize the base class for the declarative model
-BASE = declarative_base()
+# Define the database URL
+DB_URL = "asyncpg+postgresql://postgres.lfxzalulgmqdalctysxe:PkAkZwH2yHnweNsT@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
 
-def start_session() -> scoped_session:
-    """
-    Start and return a scoped session with the database.
-    """
-    engine = create_engine(DB_URL, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    session = scoped_session(sessionmaker(bind=engine, autoflush=False))
-    return session
+# Create the async engine
+engine = create_async_engine(DB_URL, echo=True)
 
-# Create a scoped session
-SESSION = start_session()
+# Define the base class for declarative models
+Base = DeclarativeBase()
